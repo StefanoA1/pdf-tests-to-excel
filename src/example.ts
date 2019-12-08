78549970535218629ebf5641a9656bc72e2a12d3
stefano <stefanoacosta.92@gmail.com>
2019-12-08 23:23:21 +0100

    feat: example

---
+++ src/example.ts
@@ -0,0 +1,257 @@
+// tslint:disable: no-trailing-whitespace
+const text: string = `
+INFORME ANALÍTICO DE MUESTRAS - FINAL
+18/10/2019  15:38:58Página 1/4
+HOSPITAL GENERAL DEL IESS QUITO SUR
+Versión del EpiCenter: V7.00A / V6.41A
+DIR: AV. PINLLOPACTA S/N Y MORASPUNGO
+TELF: 023982700
+QUITO - ECUADOR
+Nombre del paciente:
+Espinosa Cevallos Ma
+ID del paciente:173660
+FemeninoSexo del paciente:09/12/1941Fecha de nacimiento:
+Sin especificar
+Médico/petición:
+Nombre del usuario:fespinoza
+1485725N° de acceso:
+OrinaTipo de muestra:
+HemodiálisisServicio de hospital:
+Zona del cuerpo:
+Sin especificar
+09/05/2019  09:29:01Fecha de recibo:09/05/2019  09:29:01Fecha de recogida:
+Antibioterapia:
+Número de aislado:Ninguno
+Crecimiento y detecciónTipos de tests:
+Nombre del test:UROCULTIVOEstatus:POSITIVO
+N° de secuencia:Fecha/hora de inicio :10/05/2019  07:41:43
+Ubicación:
+Fecha/Hora de resultado:10/05/2019  07:42:40
+
+INFORME ANALÍTICO DE MUESTRAS - FINAL
+18/10/2019  15:38:58Página 2/4
+HOSPITAL GENERAL DEL IESS QUITO SUR
+Versión del EpiCenter: V7.00A / V6.41A
+DIR: AV. PINLLOPACTA S/N Y MORASPUNGO
+TELF: 023982700
+QUITO - ECUADOR
+Nombre del paciente:
+Espinosa Cevallos Ma
+ID del paciente:173660
+FemeninoSexo del paciente:09/12/1941Fecha de nacimiento:
+Sin especificar
+Médico/petición:
+Nombre del usuario:fespinoza
+1485725N° de acceso:
+OrinaTipo de muestra:
+HemodiálisisServicio de hospital:
+Zona del cuerpo:
+Sin especificar
+09/05/2019  09:29:01Fecha de recibo:09/05/2019  09:29:01Fecha de recogida:
+Antibioterapia:
+Número de aislado:1
+Final
+Escherichia coliNombre del organismo:
+Significativo/Adquirido en comunidadClasificación del aislado:
+Previamente conocido como:
+"Bacterium coli", "Bacillus coli", "Bacterium coli commune"
+Es un comensal entérico habitual de humanos y animales. Es el patógeno humano más común. Es la 
+causa más frecuente de infecciones de las vías urinarias, bacteriemia y la diarrea bacteriana del 
+turista. Es una causa importante de la meningitis neonatal y otras infecciones, incluyendo la 
+neumonía.
+Notas taxonómicas:
+UROCULTIVOOrigen del aislado:10/05/2019Fecha de resultado:
+Resultados AST del aislado
+Antibiótico
+CMI o 
+Concentración
+Interp.
+SIR 
+Experto
+SIR 
+final
+Número 
+regla
+Grupo de 
+test de 
+antibiótico
+SBS<=8Amikacina
+RB
+ 1407
+RS8/4Amoxicilina-Clavulanato
+RAR>16Ampicilina
+RCR>16Aztreonam
+RAR>16Cefalotina
+RAR>16Cefepima
+IBI16Cefoxitina
+RA
+ 1529
+RI8Ceftazidima
+RAR>32Ceftriaxona
+RBR>16Cefuroxima
+SBS<=0,5Ciprofloxacino
+XO
+ 1551
+XS<=1Colistina
+SBI1Ertapenem
+SAS<=2Gentamicina
+SAS<=1Imipenem
+SBS<=1Levofloxacino
+SAS<=1Meropenem
+SUS<=16Nitrofurantoína
+SBS<=4/4Piperacilina-Tazobactam
+SAS<=1Tigeciclina
+SBS<=1/19Trimetoprim-Sulfametoxazol
+Marcadores de resistencia
+Betalactamasa de Amplio EspectroESBLRegla 1505
+Productor potencial de carbapenemasaALERT1
+Betalactamasa tipo AMPC PlasmidicaAMPCpRegla 8001
+Reglas desencadenadas de Experto
+
+INFORME ANALÍTICO DE MUESTRAS - FINAL
+18/10/2019  15:38:58Página 3/4
+HOSPITAL GENERAL DEL IESS QUITO SUR
+Versión del EpiCenter: V7.00A / V6.41A
+DIR: AV. PINLLOPACTA S/N Y MORASPUNGO
+TELF: 023982700
+QUITO - ECUADOR
+Nombre del paciente:
+Espinosa Cevallos Ma
+ID del paciente:173660
+FemeninoSexo del paciente:09/12/1941Fecha de nacimiento:
+Sin especificar
+Médico/petición:
+Nombre del usuario:fespinoza
+1485725N° de acceso:
+OrinaTipo de muestra:
+HemodiálisisServicio de hospital:
+Zona del cuerpo:
+Sin especificar
+09/05/2019  09:29:01Fecha de recibo:09/05/2019  09:29:01Fecha de recogida:
+Antibioterapia:
+Reglas desencadenadas de Experto
+El aislado se confirma como positivo para ESBL. Avise al médico y al especialista en 
+control de infecciones. Verifique los resultados si son poco frecuentes.
+AutomáticoRegla 1505
+Cuando se detecta una ESBL, una interpretación de sensible o intermedio para las 
+penicilinas, las cefalosporinas (distintas de las cefamicinas) y el aztreonam se registra 
+como resistente.
+AutomáticoRegla 1529
+Cuando se detecta una ESBL, una interpretación de sensible o intermedio para 
+amoxicilina-clavulanato se registra como resistente.
+AutomáticoRegla 1407
+Realice un método de análisis alternativo antes de registrar resultados sensibles a 
+colistina. El resultado SIR se ha suprimido.
+AutomáticoRegla 1551
+AMPc PlasmidicaAutomáticoRegla 8001
+BLEESAutomáticoRegla 8002
+Combo ID/ASTTipos de tests:
+Nombre del test:NMIC/ID-94Estatus:Completo
+N° de secuencia:427813727376Fecha/hora de inicio :09/05/2019  09:29:01N° de lote:9058686
+Ubicación:2/A15
+Fecha/Hora de resultado:09/05/2019  23:48:23
+Nombre del organismo:Escherichia coliDensidad inóculo:0.5
+Resultados ID de instrumento
+Valor 
+confianza
+Escherichia coli99%
+Substancia 
+bioquímica
+Result. 
+Instrum
+Result. 
+esperado
+Result. 
+Instrum
+Substancia 
+bioquímica
+Result. 
+esperado
+Result. 
+Instrum
+Substancia 
+bioquímica
+Result. 
+esperado
+123123123
+A_ARARR+
+±A_GLPRB--A_GLYB--
+A_GUGAH--A_LARGH+-A_LGTA+±
+A_LLEUH+±A_LPHET--A_LPROB--
+A_LPYR--A_LTRY--A_LYALD+±
+C_ACT--C_ADO--C_CIT--
+C_CLST-±C_DMNT+±C_KGA-±
+C_MLO--C_PXB-±C_TIG--
+M_NAG--N_LGGH--N_LPROT+±
+P_BDGLU--P_BPHO-±R_BALL-±
+R_BGEN--R_DEX++R_DFRU++
+R_DGAL+±R_DGUA++R_DMLB+±
+R_DSBT+±R_DSUC--R_GRA++
+
+INFORME ANALÍTICO DE MUESTRAS - FINAL
+18/10/2019  15:38:58Página 4/4
+HOSPITAL GENERAL DEL IESS QUITO SUR
+Versión del EpiCenter: V7.00A / V6.41A
+DIR: AV. PINLLOPACTA S/N Y MORASPUNGO
+TELF: 023982700
+QUITO - ECUADOR
+Nombre del paciente:
+Espinosa Cevallos Ma
+ID del paciente:173660
+FemeninoSexo del paciente:09/12/1941Fecha de nacimiento:
+Sin especificar
+Médico/petición:
+Nombre del usuario:fespinoza
+1485725N° de acceso:
+OrinaTipo de muestra:
+HemodiálisisServicio de hospital:
+Zona del cuerpo:
+Sin especificar
+09/05/2019  09:29:01Fecha de recibo:09/05/2019  09:29:01Fecha de recogida:
+Antibioterapia:
+Substancia 
+bioquímica
+Result. 
+Instrum
+Result. 
+esperado
+Result. 
+Instrum
+Substancia 
+bioquímica
+Result. 
+esperado
+Result. 
+Instrum
+Substancia 
+bioquímica
+Result. 
+esperado
+123123123
+R_LARA++R_LRHA-±R_MBGU--
+R_MTU--R_NGA+±R_NGU++
+S_ORN-±S_URE--T_ESC--
+`;
+
+const regex = new RegExp(
+  // tslint:disable-next-line: trailing-comma
+  /Grupo\s*de\s*\s*test\s*de\s*antibiótico([\w\W]*)Marcadores\s*de\s*resistencia\s*/
+);
+const match = regex.exec(text);
+const antibiotics = match && match[1];
+// console.log(antibiotics);
+
+const regexAntib = new RegExp(
+  // tslint:disable-next-line: trailing-comma
+  /[SRIX]\s*[ABCOU]\s*\d*\s*[RX]?\s*[SRI]\s*([<>=]{0,2}[/\d,]+)([A-Z][a-zí]+[A-Z-]{0,2}[a-zí]+)\s*/g
+);
+let antibMatch = regexAntib.exec(antibiotics!);
+
+const antibMatches = [];
+while (antibMatch != null) {
+  antibMatch.shift();
+  antibMatches.push(antibMatch);
+  antibMatch = regexAntib.exec(antibiotics!);
+}
+
+console.log(antibMatches);
